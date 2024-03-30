4a2c496eab5792922189b4c0ecc0cb6609064747
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-30 16:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const btn = document.querySelector("#btn");
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    let text = document.querySelector("#text").value;
+    let delay = parseInt(document.querySelector("#delay").value);
+    setTimeout(() => {
+      resolve(text);
+    }, delay);
+  });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  const result = await createPromise();
+  console.log(result);
+  document.querySelector("#output").textContent = `${result}`;
+  // Expected output: "resolved"
+}
+
+btn.addEventListener("click", asyncCall);
